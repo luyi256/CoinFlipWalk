74791ef2d44aab48db94ab449b3801959deaa4eb
beenleyi <iyibeenle@github.com>
2022-03-30 15:38:06 +0000

    一个算法一个文件

--- MCSS.cpp
+++ MCSS.cpp
@@ -1,187 +1,239 @@
-class Graph
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include <sys/time.h>
+#include <unordered_map>
+#include <chrono>
+#include "Graph.h"
+using namespace std;
+typedef unsigned int uint;
+#include "utils.h"
+int main(int argc, char **argv)
 {
-public:
-    uint n; //number of nodes
-    uint m; //number of edges
-    string filedir;
-    string filelabel;
-    double *totw;
-    uint *out_size_set;
-    unordered_map<uint, node *> *umap;       //<neighbor_id, nodeptr> umap[source_id]
-    unordered_map<uint, node *> *chain_head; //<low_bound,chain_head_ptr> chain_head[source_id]
-    Graph(string _filedir, string _filelabel);
-    void add(uint s, uint t, double w);
-    void modify(uint s, uint t, double w);
-    void del(uint s, uint t);
-    ~Graph()
+    char *endptr;
+    long querynum = 10;
+    vector<double> epss;
+    uint L = 10;
+    string filedir, filelabel;
+    argParser(argc, argv, filedir, filelabel, querynum, epss, L);
+    subsetGraph g(filedir, filelabel);
+    g.update();
+    string queryname;
+    queryname = "./query/" + filelabel + ".query";
+    ifstream query;
+    cout << "Input query file from: " << queryname << endl;
+    double *final_p = new double[g.n];
+    uint *final_node = new uint[g.n];
+    uint *final_exist = new uint[g.n];
+    uint final_count = 0;
+    uint *cs_exist[2];
+    cs_exist[0] = new uint[g.n];
+    cs_exist[1] = new uint[g.n];
+    //当前层candidate_set的点
+    uint *candidate_set[2];
+    candidate_set[0] = new uint[g.n];
+    candidate_set[1] = new uint[g.n];
+    uint candidate_count[2];
+    candidate_count[0] = 0;
+    candidate_count[1] = 0;
+    //当前层该点的probability
+    double *prob[2];
+    prob[0] = new double[g.n];
+    prob[1] = new double[g.n];
+    for (uint i = 0; i < g.n; i++)
     {
+        cs_exist[0][i] = 0;
+        cs_exist[1][i] = 0;
+        candidate_set[0][i] = 0;
+        candidate_set[1][i] = 0;
+        prob[0][i] = 0;
+        prob[1][i] = 0;
+        final_p[i] = 0;
+        final_node[i] = 0;
+        final_exist[i] = 0;
     }
-};
-struct node
-{
-    node *pre;
-    node *post;
-    uint idx;
-    double w;
-};
-
-void Graph::add(uint s, uint t, double w)
-{
-    unordered_map<uint, node *>::const_iterator got;
-    got = umap[s].find(t);
-    if (got != umap[s].end())
-        return; //已存在
-    node *post_node, *cur_node;
-    int listnum = ceil(log2(w));
-    if (listnum <= 0)
-        listnum = 0;
-    got = chain_head[s].find(listnum);
-    if (got != chain_head[s].end())
+    stringstream ss_run;
+    ss_run << "./analysis/MCSS_" << filelabel << "_runtime.csv";
+    ofstream writecsv;
+    writecsv.open(ss_run.str(), ios::app);
+    for (auto epsIt = epss.begin(); epsIt != epss.end(); epsIt++)
     {
-        post_node = got->second;
-        cur_node = new node{
-            NULL,
-            post_node,
-            t,
-            w};
-        post_node->pre = cur_node;
-    }
-    else
-    {
-        cur_node = new node{
-            NULL,
-            NULL,
-            t,
-            w};
-    }
-    chain_head[s][listnum] = cur_node;
-    umap[s][t] = cur_node;
-    out_size_set[s]++;
-    totw[s] += w;
-}
-
-void Graph::modify(uint s, uint t, double w)
-{
-    unordered_map<uint, node *>::const_iterator got;
-    got = umap[s].find(t);
-    if (got == umap[s].end())
-        return; //不存在
-    totw[s] += w - umap[s][t]->w;
-    umap[s][t]->w = w;
-}
-
-void Graph::del(uint s, uint t)
-{
-    unordered_map<uint, node *>::const_iterator got;
-    got = umap[s].find(t);
-    if (got == umap[s].end())
-        return; //不存在
-    node *cur_node;
-    cur_node = got->second;
-    if (cur_node->pre == NULL)
-    {
-        int listnum = ceil(log2(cur_node->w));
-        if (listnum <= 0)
-            listnum = 0;
-        node *head = cur_node->post;
-        chain_head[s][listnum] = head;
-        if (head != NULL)
-            head->pre = NULL;
-    }
-    else
-    {
-        cur_node->pre->post = cur_node->post;
-        if (cur_node->post != NULL)
-            cur_node->post->pre = cur_node->pre;
-    }
-
-    totw[s] -= cur_node->w;
-    out_size_set[s]--;
-    free(cur_node);
-}
-
-Graph::Graph(string _filedir, string _filelabel)
-{
-    filedir = _filedir;
-    filelabel = _filelabel;
-    stringstream ss_dir, ss_attr, ss_outEL, ss_outPL, ss_outWEL, ss_inEL, ss_inPL, ss_inWEL;
-    uint *outEL;
-    uint *outPL;
-    double *outWEL;
-    ss_attr << filedir << filelabel << ".attribute";
-    ifstream in_attr;
-    in_attr.open(ss_attr.str());
-    cout << "FilePath: " << ss_attr.str() << endl;
-    cout << "Read graph attributes..." << endl;
-    string tmp;
-    in_attr >> tmp >> n;
-    in_attr >> tmp >> m;
-    cout << "n=" << n << " m=" << m << endl;
-
-    in_attr.close();
-
-    cout << "Read graph edges..." << endl;
-
-    ss_outEL << filedir << filelabel << ".outEdges";
-    ss_outWEL << filedir << filelabel << ".outWEdges";
-
-    ss_outPL << filedir << filelabel << ".outPtr";
-
-    outEL = new uint[m];
-    outPL = new uint[n + 1];
-    outWEL = new double[m];
-    //每个节点的出节点下标从哪里开始
-    ifstream outpf(ss_outPL.str(), ios::in | ios::binary);
-    outpf.read((char *)&outPL[0], sizeof(outPL[0]) * (n + 1));
-    //所有节点的出节点编号
-    ifstream outf, outef;
-    outf.open(ss_outEL.str(), ios::in | ios::binary);
-    outf.read((char *)&outEL[0], sizeof(outEL[0]) * m);
-    outef.open(ss_outWEL.str(), ios::in | ios::binary);
-    outef.read((char *)&outWEL[0], sizeof(outWEL[0]) * m);
-    outf.close();
-    outpf.close();
-    outef.close();
-    chain_head = new unordered_map<uint, node *>[n];
-    umap = new unordered_map<uint, node *>[n];
-    totw = new double[n];
-    out_size_set = new uint[n];
-    for (int i = 0; i < n; i++)
-    {
-        node *post_node, *cur_node;
-        uint outSize = (outPL[i + 1] - outPL[i]);
-        out_size_set[i] = outSize;
-        totw[i] = 0;
-        unordered_map<uint, node *>::const_iterator got;
-        for (int j = 0; j < outSize; j++)
+        double eps = *epsIt;
+        query.open(queryname);
+        double avg_time = 0;
+        for (uint i = 0; i < querynum; i++)
         {
-            uint idx = outEL[(outPL[i] + j)];
-            double w = outWEL[(outPL[i] + j)];
-            totw[i] += w;
-            int listnum = ceil(log2(w));
-            if (listnum <= 0)
-                listnum = 0;
-            got = chain_head[i].find(listnum);
-            if (got != chain_head[i].end())
+            Random R;
+            uint u;
+            query >> u;
+            cout << i << ": " << u << endl;
+            clock_t t0 = clock();
+            uint cnt1, cnt2;
+            uint levelID = L % 2;
+            for (uint j = 0; j < final_count; j++) // hanzhi
             {
-                post_node = got->second;
-                cur_node = new node{
-                    NULL,
-                    post_node,
-                    idx,
-                    w};
-                post_node->pre = cur_node;
+                final_p[final_node[j]] = 0;
+                final_exist[final_node[j]] = 0;
             }
-            else
+            final_count = 0;
+            uint nr = 0.1 * L / eps * 4; // hanzhi
+            cout << "samples=" << nr << endl;
+            for (uint k = 0; k < nr; k++) // hanzhi
             {
-                cur_node = new node{
-                    NULL,
-                    NULL,
-                    idx,
-                    w};
+                uint tempLevel = 0;
+                candidate_set[0][0] = u; // hanzhi
+                candidate_count[0] = 1;  // hanzhi
+                candidate_count[1] = 0;  // hanzhi
+                prob[0][u] = 1.0;        // hanzhi
+                while (tempLevel <= L)
+                {
+                    uint tempLevelID = tempLevel % 2;
+                    uint newLevelID = (tempLevel + 1) % 2;
+                    uint candidateCnt = candidate_count[tempLevelID];
+                    if (candidateCnt == 0)
+                        break;
+                    candidate_count[tempLevelID] = 0;
+                    for (uint j = 0; j < candidateCnt; j++)
+                    {
+                        uint tempNode = candidate_set[tempLevelID][j];
+                        double tempP = prob[tempLevelID][tempNode];
+                        cs_exist[tempLevelID][tempNode] = 0;
+                        prob[tempLevelID][tempNode] = 0;
+
+                        if (tempLevel == L)
+                        {
+                            if (final_exist[tempNode] == 0)
+                            {
+                                final_exist[tempNode] = 1;
+                                final_node[final_count++] = tempNode;
+                            }
+                            final_p[tempNode] += tempP / (double)nr;
+                            continue;
+                        }
+                        uint outSize = g.outSizeList[tempNode];
+                        double outVertWt = g.outWeightList[tempNode];
+                        double incre = tempP / outVertWt;
+                        double thetad = 0.75;
+                        for (auto setIt = g.neighborList[tempNode].begin(); setIt != g.neighborList[tempNode].end(); setIt++)
+                        {
+                            int setID = setIt->first;
+                            double increMax = incre * (pow(2, setID));
+                            double pmax = pow(2, setID) / outVertWt; // the maximum sampling probability in this subset;
+                            if (increMax >= thetad)
+                            {
+                                for (auto nodeIt = setIt->second.begin(); nodeIt != setIt->second.end(); nodeIt++)
+                                {
+                                    uint newNode = nodeIt->id;
+                                    prob[newLevelID][newNode] += incre * nodeIt->w;
+                                    if (cs_exist[newLevelID][newNode] == 0)
+                                    {
+                                        cs_exist[newLevelID][newNode] = 1;
+                                        candidate_set[newLevelID][candidate_count[newLevelID]++] = newNode;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                uint subsetSize = setIt->second.size();
+                                // cout<<"subsetSize="<<subsetSize<<endl;//hanzhi
+                                int seed = chrono::system_clock::now().time_since_epoch().count();
+                                std::default_random_engine generator(seed);
+                                std::binomial_distribution<int> distribution(subsetSize, increMax / thetad);
+                                double rbio = distribution(generator);
+                                for (uint j = 0; j < rbio; j++)
+                                {
+                                    double r1 = floor(R.drand() * subsetSize);
+                                    node tmp = setIt->second[r1];
+                                    double r2 = R.drand();
+                                    if (r2 < tmp.w / pow(2, setID))
+                                    {
+                                        prob[newLevelID][tmp.id] += 1.0;
+                                        if (cs_exist[newLevelID][tmp.id] == 0)
+                                        {
+                                            cs_exist[newLevelID][tmp.id] = thetad;
+                                            candidate_set[newLevelID][candidate_count[newLevelID]++] = tmp.id;
+                                        }
+                                    }
+                                }
+
+                                // int curti = 0;
+                                // while (curti < subsetSize)
+                                // {
+                                // 	int seed_geo = chrono::system_clock::now().time_since_epoch().count(); // hanzhi
+                                // 	std::default_random_engine generator(seed_geo);
+                                // 	std::geometric_distribution<int> distribution(pmax); // hanzhi
+                                // 	int rgeo = distribution(generator);					 // hanzhi
+                                // 	curti += rgeo;
+                                // 	if (curti >= subsetSize)
+                                // 	{		   // hanzhi
+                                // 		break; // hanzhi
+                                // 	}		   // hanzhi
+                                // uint nodeidx = curti;
+                                // double r = R.drand();
+                                // node tmp = setIt->second[nodeidx];
+                                // if (r < tmp.w / pow(2, setID))
+                                // {
+                                // 	prob[newLevelID][tmp.id] += tempP;
+                                // 	if (cs_exist[newLevelID][tmp.id] == 0)
+                                // 	{
+                                // 		cs_exist[newLevelID][tmp.id] = 1;
+                                // 		candidate_set[newLevelID][candidate_count[newLevelID]++] = tmp.id;
+                                // 	}
+                                // }
+                                // curti += 1; // hanzhi
+                            }
+                        }
+                    }
+                    tempLevel++;
+                }
             }
-            chain_head[i][listnum] = cur_node;
-            umap[i][idx] = cur_node;
+            clock_t t1 = clock();
+            avg_time += (t1 - t0) / (double)CLOCKS_PER_SEC;
+            cout << "Query time for node " << u << ": " << (t1 - t0) / (double)CLOCKS_PER_SEC << " s" << endl;
+
+            stringstream ss_dir, ss;
+
+            ss_dir << "./result/MCSS/" << filelabel << "/" << L << "/" << eps << "/";
+            ss << ss_dir.str() << u << ".txt";
+            cout << "Write query results in file: " << ss.str() << endl;
+            mkpath(ss_dir.str());
+            ofstream fout;
+            fout.open(ss.str());
+            fout.setf(ios::fixed, ios::floatfield);
+            fout.precision(15);
+            if (!fout)
+                cout << "Fail to open the writed file" << endl;
+            cout << "final_count=" << final_count << endl;
+            for (uint j = 0; j < final_count; j++)
+            {
+                fout << final_node[j] << " " << final_p[final_node[j]] << endl;
+            }
+            fout.close();
         }
+        query.close();
+        cout << endl;
+        cout << "query time: " << avg_time / (double)querynum << " s" << endl;
+        cout << "==== "
+             << "MCSS"
+             << " with " << eps << " on " << filelabel << " done!====" << endl;
+        writecsv << avg_time / (double)querynum << ',';
     }
+
+    delete[] cs_exist[0];
+    delete[] cs_exist[1];
+    delete[] candidate_set[0];
+    delete[] candidate_set[1];
+    delete[] prob[0];
+    delete[] prob[1];
+    delete[] final_p;
+    delete[] final_node;
+    delete[] final_exist;
+    cout << endl
+         << endl
+         << endl;
+
+    writecsv.close();
 }
