72deca56cd395ee0605b3d666fbae5d1879da38e
beenleyi <iyibeenle@github.com>
2022-09-17 07:13:50 +0000

    update的画图合成一个文件

--- plot.py
+++ plot.py
@@ -1,13 +1,90 @@
 from turtle import color
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from datetime import datetime
+import pytz
+import os
+# args
+# 选择提取哪些数据集，会往本地写数据，文件：'.datalog/[date]_[measurement].data'
+datasets = ["twitter-2010", "threads-stack-overflow"]
+datasets_alias = ['TW', 'TH']
+# "threads-stack-overflow" "colisten-Spotify" "bitcoin-temporal" "indochina-2004" "twitter-2010"
+log_measures = ['memory', 'memory_overhead', 'time', 'time_overhead']
+plot_measures = ['memory', 'memory_overhead', 'time', 'time_overhead']
 
-# strs = ["memory_overhead", "memory", "time", 'time_overhead']
-strs = ["time"]
-dataset = ['BT', 'CS', 'IC', 'TH']
-algo = ['AliasWalk', 'CoinFlipWalk', 'PrefixWalk', 'RejectionWalk']
-num_dataset = len(dataset)
+# algos = ['AliasWalk', 'CoinFlipWalk', 'PrefixWalk', 'RejectionWalk']
+algos = ['MCAM', 'MCSS', 'MCPS', 'MCAR']
+algo_alias = ['AliasWalk', 'CoinFlipWalk', 'PrefixWalk', 'RejectionWalk']
+
+tz = pytz.timezone('Asia/Shanghai')
+# 避免生成的pdf在苹果系统中无法正确显示线形
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+
+# RejectionWalk的位置，求overhead的时候需要减去
+to_sub = 0
+for i, algo in enumerate(algos):
+    if algo == 'MCAR' or algo == 'RejectionWalk':
+        to_sub = i
+
+time = []
+memory = []
+for dataset in datasets:
+    tmp_time = []
+    tmp_memory = []
+    for algo in algos:
+        filename = './runlog/' + algo + '_' + dataset + '.log'
+        tmp_del_time = 0
+        tmp_add_time = 0
+        with open(filename, 'r') as f:
+            for i, line in enumerate(f.readlines()):
+                if i == 4:  # del time
+                    tmp_del_time = float(line.split()[5])
+                if i == 5:
+                    tmp_add_time = float(line.split()[5])
+                if i == 6:
+                    tmp_memory.append(float(line.split()[4]))
+                    break
+        tmp_time.append((tmp_del_time + tmp_add_time) / 2)
+    time.append(tmp_time)
+    memory.append(tmp_memory)
+print(time)
+print(memory)
+time_overhead = [[j - i[to_sub] for j in i] for i in time]
+memory_overhead = [[j - i[to_sub] for j in i] for i in memory]
+print(time_overhead)
+print(memory_overhead)
+
+dt = datetime.now(tz).strftime("%Y-%m-%d %H:%M")
+path = "./datalog/" + dt + '/'
+if (os.path.exists(path) == False):
+    os.makedirs(path)
+for measure in log_measures:
+    isTime = False if measure.find("time") == -1 else True
+    isOverhead = False if measure.find("overhead") == -1 else True
+    if isTime and not isOverhead:
+        filename = dt + "_" + "time.data"
+        arr = time
+    elif isTime and isOverhead:
+        filename = dt + "_" + "time_overhead.data"
+        arr = time_overhead
+    elif not isTime and not isOverhead:
+        filename = dt + "_" + "memory.data"
+        arr = memory
+    elif not isTime and isOverhead:
+        filename = dt + "_" + "memory_overhead.data"
+        arr = memory_overhead
+
+    f = open(path + filename, 'w')
+    for i, subarr in enumerate(arr):
+        f.write(datasets[i] + ':')
+        f.write(str(subarr))
+        f.write('\n')
+    f.close()
+
+num_dataset = len(datasets)
 # colors = [(0, 0.4470, 0.7410), (0.8500, 0.3250, 0.0980),
 #           (0.4940, 0.1840, 0.5560), (0.6353, 0.07843, 0.1843)]
 colors = ['#263FA9', '#447B48', '#E85472', '#A686EC']
@@ -15,22 +92,25 @@ colors = ['#263FA9', '#447B48', '#E85472', '#A686EC']
 #EC7F4B
 #263FA9
 #447B48
-for str in strs:
-    isTime = False if str.find("time") == -1 else True
-    isOverhead = False if str.find("overhead") == -1 else True
-    file = './fig/' + str + '.txt'
-    table = pd.read_csv(file, header=None)
-    array = np.array(table)
-    newarr = np.array([array[2, :], array[3, :], array[1, :], array[0, :]])
+for measure in plot_measures:
+    isTime = False if measure.find("time") == -1 else True
+    isOverhead = False if measure.find("overhead") == -1 else True
+    if isTime and not isOverhead:
+        arr = time
+    elif isTime and isOverhead:
+        arr = time_overhead
+    elif not isTime and not isOverhead:
+        arr = memory
+    elif not isTime and isOverhead:
+        arr = memory_overhead
     for k in range(num_dataset):
         fig, ax = plt.subplots(figsize=(10, 5))
-        print(type(newarr[0, k]))
-        ax.bar(algo,
-               newarr[:, k],
-               tick_label=algo,
+        ax.bar(algos,
+               arr[k],
+               tick_label=algo_alias,
                color='none',
                log=isTime,
-               lineWidth=3)
+               linewidth=3)
         ax.tick_params(axis='x', labelsize=25, rotation=15)
         plt.yticks(fontsize=18)
         if isTime and isOverhead:
@@ -42,12 +122,12 @@ for str in strs:
         else:
             ax.set_ylabel("memory (GB)", fontsize=25)
         hatches = ["/", "x", ".", "\\"]
-        ax.set_title(dataset[k], fontsize=25)
+        ax.set_title(datasets_alias[k], fontsize=25)
         for i, patch in enumerate(ax.patches):
             patch.set_hatch(hatches[i])
             patch.set_edgecolor(colors[i])
         plt.rcParams['hatch.linewidth'] = 3.0
         plt.tight_layout()
 
-        plt.savefig("./fig/{}_{}.png".format(str, dataset[k]))
-        plt.savefig("./fig/{}_{}.pdf".format(str, dataset[k]))
+        plt.savefig(path + "{}_{}.png".format(measure, datasets_alias[k]))
+        plt.savefig(path + "{}_{}.pdf".format(measure, datasets_alias[k]))
