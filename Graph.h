124a90ceeb8a0c3c9960b8f7f11e3e0c9a67e3c3
beenleyi <iyibeenle@github.com>
2022-09-23 07:34:18 +0000

    cherry-pick ad034ef 9201ba2 Reject:add maxw; CFW: ceil

--- Graph.h
+++ Graph.h
@@ -50,6 +50,7 @@ public:
 	unordered_map<uint, vector<node>> neighborList;
 	unordered_map<uint, unordered_map<uint, int>> adjList;
 	unordered_map<uint, uint> outSizeList;
+	unordered_map<uint, double> maxWeight;
 	Graph() {}
 	Graph(const string &_filedir, const string &_filelabel)
 	{
@@ -229,16 +230,20 @@ public:
 			neiNumIn.read(reinterpret_cast<char *>(&outSizeSum), sizeof(uint));
 			outSize = outSizeSum - preOutSizeSum;
 			preOutSizeSum = outSizeSum;
+			double maxw = 0;
 			for (uint j = 0; j < outSize; j++)
 			{
 				uint id;
 				double w;
 				neiNodeIn.read(reinterpret_cast<char *>(&id), sizeof(uint));
 				neiWeightIn.read(reinterpret_cast<char *>(&w), sizeof(double));
+				if (w > maxw)
+					maxw = w;
 				neighborList[i].push_back(node(id, w));
 				adjList[i][id] = j;
 			}
 			outSizeList[i] = outSize;
+			maxWeight[i] = maxw;
 		}
 		neiNumIn.close();
 		neiWeightIn.close();
@@ -822,7 +827,7 @@ public:
 			// int gap = floor(log2(outWeight / (outSize * outSize))) + 1;
 			for (uint j = 0; j < outSize; j++)
 			{
-				int subset = pow(2,floor(log2(neighbor[j].w)) + 1);
+				int subset = pow(2, ceil(log2(neighbor[j].w)));
 				// if (subset < gap && smallSetMap[i].find(subset) == smallSetMap[i].end())
 				// {
 				// 	smallSetID *newSmallSet = new smallSetID();
@@ -936,7 +941,7 @@ public:
 		{
 			uint s = sarr[i];
 			uint t = tarr[i];
-			int subset = pow(2,floor(log2(warr[i])) + 1);
+			int subset = pow(2, ceil(log2(warr[i])));
 			// double oldGap = floor(log2(outWeightList[s] / (outSizeList[s] * outSizeList[s]))) + 1;
 			neighborList[s][subset].push_back(node(t, warr[i]));
 			outSizeList[s]++;
