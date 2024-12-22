4fab2df8cf44f381f80a590860d697bd2840c63b
beenleyi <iyibeenle@gmail.com>
2024-12-21 19:34:27 -0700

    delete useless files

--- Graph.h
+++ Graph.h
@@ -15,11 +15,8 @@
 #include <errno.h>
 #include <math.h>
 #include "alias.h"
-#include "BIT.h"
 #include "AVL.h"
-#include "rbtree.h"
 #include "utils.h"
-#include "Priority.h"
 #include <chrono>
 #include<stdio.h>
 using namespace std;
@@ -607,160 +604,6 @@ public:
 	}
 };
 
-// class BITPrefixSumGraph : public Graph
-// {
-// public:
-// 	unordered_map<uint, BIT> BITList;
-// 	void add(uint s, uint t, double w)
-// 	{
-// 		Graph::add(s, t, w);
-// 		BITList[s].add(w);
-// 	}
-// 	BITPrefixSumGraph(const string &_filedir, const string &_filelabel) : Graph(_filedir, _filelabel)
-// 	{
-// 		for (uint i = 0; i < n; i++)
-// 		{
-// 			uint outSize = outSizeList[i];
-// 			double *arr = new double[outSize];
-// 			for (uint j = 0; j < outSize; j++)
-// 				arr[j] = neighborList[i][j].w;
-// 			BITList[i] = BIT(outSize, arr);
-// 		}
-// 	}
-// 	BITPrefixSumGraph() {}
-// 	~BITPrefixSumGraph() {}
-// };
-
-class BSTPrefixSumGraph
-{
-public:
-	unordered_map<uint, bst> BSTList;
-	uint n;
-	string filedir, filelabel;
-	unordered_map<uint, vector<node>> neighborList;
-	unordered_map<uint, unordered_map<uint, int>> adjList;
-	unordered_map<uint, uint> outSizeList;
-	unordered_map<uint, double> outWeightList;
-	BSTPrefixSumGraph(const string& _filedir, const string& _filelabel)
-	{
-		filedir = _filedir;
-		filelabel = _filelabel;
-		string neiNode, neiWeight, neiNum, graphAttr;
-		neiNode = filedir + filelabel + ".outEdges";
-		neiWeight = filedir + filelabel + ".outWEdges";
-		neiNum = filedir + filelabel + ".outPtr";
-		graphAttr = filedir + filelabel + ".attribute";
-		cout << "FilePath: " << graphAttr.c_str() << endl;
-		readFile(graphAttr, neiNode, neiWeight, neiNum);
-	}
-
-	void update()
-	{
-		ifstream opfile(filedir + "/" + filelabel + ".op", ios::in);
-		if (!opfile)
-		{
-			getAddEdge(add_num);
-			opfile.open(filedir + "/" + filelabel + ".op", ios::in);
-		}
-		uint sarr[add_num];
-		uint tarr[add_num];
-		double warr[add_num];
-		int opnum = 0;
-		while (opfile >> sarr[opnum] >> tarr[opnum] >> warr[opnum])
-		{
-			opnum++;
-		}
-		opfile.close();
-		// delete
-		// auto begin = chrono::high_resolution_clock::now();
-		// for (int i = 0; i < add_num; i++)
-		// {
-		// 	uint s = sarr[i];
-		// 	uint t = tarr[i];
-		// 	uint neiidx = adjList[s][t];
-		// 	uint outsize = outSizeList[s];
-		// 	if (neiidx == outsize - 1)
-		// 		neighborList[s].pop_back();
-		// 	else
-		// 	{
-		// 		auto tmp = neighborList[s].end() - 1;
-		// 		neighborList[s][neiidx].id = tmp->id;
-		// 		neighborList[s][neiidx].w = tmp->w;
-		// 		adjList[s][tmp->id] = neiidx;
-		// 		neighborList[s].pop_back();
-		// 		adjList[s].erase(t);
-		// 	}
-		// 	outSizeList[s]--;
-		// 	outWeightList[s] -= warr[i];
-		// 	// BST无法处理删掉一个值的情况，因为存储的是前缀和，这样前面所有值都要改变，这里应该只能通过树状数组完成
-		// 	BSTList[s].delete_value(outWeightList[s]);
-		// }
-		// cout << filelabel << "avg del time: " << (chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - begin).count() / 1000000000.0) / add_num << endl;
-		// add
-		auto begin = chrono::high_resolution_clock::now();
-		for (int i = 0; i < add_num; i++)
-		{
-			neighborList[sarr[i]].push_back(node{ tarr[i], warr[i] });
-			outSizeList[sarr[i]]++;
-			outWeightList[sarr[i]] += warr[i];
-			BSTList[sarr[i]].insert(outWeightList[sarr[i]]);
-			adjList[sarr[i]][tarr[i]] = neighborList[sarr[i]].size() - 1;
-		}
-		cout << filelabel << " new add update time: " << (chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - begin).count() / 1000000000.0) / add_num << endl;
-	}
-
-	void getAddEdge(long num)
-	{
-		cout << "No update file. Try MCAR first and then run MCPS" << endl;
-		exit(-1);
-	}
-
-	void readFile(const string& graphAttr, const string& neiNode, const string& neiWeight, const string& neiNum)
-	{
-		cout << "Read graph attributes..." << endl;
-		string tmp;
-		ifstream graphAttrIn(graphAttr.c_str());
-		graphAttrIn >> tmp >> n;
-		cout << "n=" << n << endl;
-		graphAttrIn.close();
-		cout << "Read graph ..." << endl;
-		ifstream neiNumIn(neiNum.c_str(), ios::in | ios::binary);
-		ifstream neiWeightIn(neiWeight.c_str(), ios::in | ios::binary);
-		ifstream neiNodeIn(neiNode.c_str(), ios::in | ios::binary);
-		uint outSize;
-		uint outSizeSum = 0, preOutSizeSum = 0;
-		neiNumIn.read(reinterpret_cast<char*>(&outSizeSum), sizeof(uint));
-		for (uint i = 0; i < n; i++)
-		{
-			double outWeight = 0;
-			neiNumIn.read(reinterpret_cast<char*>(&outSizeSum), sizeof(uint));
-			outSize = outSizeSum - preOutSizeSum;
-			preOutSizeSum = outSizeSum;
-			double prefixsum = 0;
-			for (uint j = 0; j < outSize; j++)
-			{
-				uint id;
-				double w;
-				neiNodeIn.read(reinterpret_cast<char*>(&id), sizeof(uint));
-				neiWeightIn.read(reinterpret_cast<char*>(&w), sizeof(double));
-				neighborList[i].push_back(node(id, w));
-				outWeight += w;
-				prefixsum += neighborList[i][j].w;
-				adjList[i][id] = j;
-				BSTList[i].insert(prefixsum);
-			}
-			outSizeList[i] = outSize;
-			outWeightList[i] = outWeight;
-		}
-		neiNumIn.close();
-		neiWeightIn.close();
-		neiNodeIn.close();
-	}
-
-	BSTPrefixSumGraph() {}
-	~BSTPrefixSumGraph() {}
-};
-
 class AVLPrefixSumGraph
 {
 public:
@@ -1117,11 +960,6 @@ public:
 	~AliasMethodGraph() {}
 };
 
-// struct smallSetID
-// {
-// 	int id;
-// 	smallSetID *next, *pre;
-// };
 class subsetGraph
 {
 public:
@@ -1238,8 +1076,6 @@ public:
 				tmpSubsetInfo.addr[tmpSubsetInfo.lastIdx].w = neighbor[j].w;
 				adjList[i][neighbor[j].id] = make_pair(subsetID, tmpSubsetInfo.lastIdx++);
 			}
-			sort(subsetWei.begin(), subsetWei.end(), greater<int>());
-			subsetHeap[i] = Priority(subsetWei);
 		}
 		neiNumIn.close();
 		neiWeightIn.close();
@@ -1312,118 +1148,3 @@ public:
 	}
 };
 
-class BITPrefixSumGraph
-{
-public:
-	unordered_map<uint, BIT> BITList;
-	uint n;
-	string filedir, filelabel;
-	unordered_map<uint, vector<node>> neighborList;
-	unordered_map<uint, uint> outSizeList;
-	unordered_map<uint, double> outWeightList;
-	BITPrefixSumGraph(const string& _filedir, const string& _filelabel)
-	{
-		filedir = _filedir;
-		filelabel = _filelabel;
-		string neiNode, neiWeight, neiNum, graphAttr;
-		graphAttr = filedir + filelabel + ".initattribute_distr";
-		ifstream graphAttrIn(graphAttr.c_str());
-		cout << "FilePath: " << graphAttr.c_str() << endl;
-		if (!graphAttrIn)
-		{
-			graphAttrIn.close();
-			cout << "NO init file. Start to construct..." << endl;
-			neiNode = filedir + filelabel + ".outEdges";
-			neiWeight = filedir + filelabel + ".outWEdges";
-			neiNum = filedir + filelabel + ".outPtr";
-			graphAttr = filedir + filelabel + ".attribute";
-			readFile(graphAttr, neiNode, neiWeight, neiNum);
-			graphAttr = filedir + filelabel + ".initattribute_distr";
-			ofstream graphAttrOut(graphAttr.c_str());
-			graphAttrOut << "n " << n;
-			graphAttrOut.close();
-			getAddEdge(10000);
-		}
-		else
-		{
-			graphAttrIn.close();
-			neiNode = filedir + filelabel + ".initoutEdges_distr";
-			neiWeight = filedir + filelabel + ".initoutWEdges_distr";
-			neiNum = filedir + filelabel + ".initoutPtr_distr";
-			readFile(graphAttr, neiNode, neiWeight, neiNum);
-		}
-	}
-
-	void update()
-	{
-		ifstream opfile(filedir + "/" + filelabel + ".op", ios::in);
-		uint sarr[add_num];
-		uint tarr[add_num];
-		double warr[add_num];
-		int opnum = 0;
-		while (opfile >> sarr[opnum] >> tarr[opnum] >> warr[opnum])
-		{
-			opnum++;
-		}
-		opfile.close();
-		auto begin = chrono::high_resolution_clock::now();
-		for (int i = 0; i < add_num; i++)
-		{
-			neighborList[sarr[i]].push_back(node{ tarr[i], warr[i] });
-			outSizeList[sarr[i]]++;
-			outWeightList[sarr[i]] += warr[i];
-			BITList[sarr[i]].add(warr[i]);
-		}
-		cout << filelabel << " new add update time: " << (chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - begin).count() / 1000000000.0) / add_num << endl;
-	}
-
-	void readFile(const string& graphAttr, const string& neiNode, const string& neiWeight, const string& neiNum)
-	{
-		cout << "Read graph attributes..." << endl;
-		string tmp;
-		ifstream graphAttrIn(graphAttr.c_str());
-		graphAttrIn >> tmp >> n;
-		cout << "n=" << n << endl;
-		graphAttrIn.close();
-		cout << "Read graph ..." << endl;
-		//每个节点的出节点下标从哪里开始
-		ifstream neiNumIn(neiNum.c_str(), ios::in | ios::binary);
-		ifstream neiWeightIn(neiWeight.c_str(), ios::in | ios::binary);
-		ifstream neiNodeIn(neiNode.c_str(), ios::in | ios::binary);
-		uint outSize;
-		uint outSizeSum = 0, preOutSizeSum = 0;
-		neiNumIn.read(reinterpret_cast<char*>(&outSizeSum), sizeof(uint));
-		for (uint i = 0; i < n; i++)
-		{
-			double outWeight = 0;
-			neiNumIn.read(reinterpret_cast<char*>(&outSizeSum), sizeof(uint));
-			outSize = outSizeSum - preOutSizeSum;
-			preOutSizeSum = outSizeSum;
-			double* arr = new double[outSize];
-
-			for (uint j = 0; j < outSize; j++)
-			{
-				uint id;
-				double w;
-				neiNodeIn.read(reinterpret_cast<char*>(&id), sizeof(uint));
-				neiWeightIn.read(reinterpret_cast<char*>(&w), sizeof(double));
-				neighborList[i].push_back(node(id, w));
-				outWeight += w;
-				arr[j] = neighborList[i][j].w;
-			}
-			BITList[i] = BIT(outSize, arr);
-			outSizeList[i] = outSize;
-			outWeightList[i] = outWeight;
-		}
-		neiNumIn.close();
-		neiWeightIn.close();
-		neiNodeIn.close();
-	}
-	void getAddEdge(long num)
-	{
-		cout << "No update file. Try MCAR first and then run MCPS_BIT" << endl;
-		exit(-1);
-	}
-	BITPrefixSumGraph() {}
-	~BITPrefixSumGraph() {}
-};
